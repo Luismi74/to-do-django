686765652677be7853e86ab6d73e3e4f56326bcf
Luis Bonilla <blessedluismi@gmail.com>
2020-12-22 00:54:48 -0400

    Update urls.py

--- backend_todo/backend_todo/urls.py
+++ backend_todo/backend_todo/urls.py
@@ -1,14 +1,11 @@
 # backend/urls.py
 import sys
-sys.path.append('to-do-django/backend_todo/todo/')
 from django.contrib import admin
 from django.urls import path, include                
 from rest_framework import routers      
-from todo import views              
+from todo import views            
                                             
 
-
-sys.path.append('../')
 
 router = routers.DefaultRouter()                     
 router.register(r'todos', views.TodoView, 'todo')     
